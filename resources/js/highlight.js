833dc575d47fd06614e35712e56dd34c29f3c2e3
Rama Can <heyramacan@gmail.com>
2024-12-23 22:31:36 +0700

    fix code highlight

--- resources/js/highlight.js
+++ resources/js/highlight.js
@@ -1,6 +1,6 @@
 import hljs from 'highlight.js';
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('livewire:navigated', () => {
     document.querySelectorAll('pre code').forEach((block) => {
         hljs.highlightElement(block);
     });
